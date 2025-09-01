6f1ba47258b5af80c8f9c8cc9335e77df1cc4e3c
Abdyl-Aziz <azizshihabov@mail.ru>
2025-09-01 18:40:29 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  let newStr = str;
+
+  for (let j = 0; j < bracketsConfig.length * 2; j += 1) {
+    for (let i = 0; i < bracketsConfig.length; i += 1) {
+      const subStr = bracketsConfig[i][0] + bracketsConfig[i][1];
+      for (let k = 0; k < str.length / 2; k += 1) {
+        newStr = newStr.replace(subStr, '');
+      }
+    }
+    if (newStr.length === 0) return true;
+  }
+
+  return false;
 };
